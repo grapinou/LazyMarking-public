b2c6450c0f00655049d175428781111cbadd4693
sighto <remigrapinou@gmail.com>
2025-10-10 10:32:56 +0200

    contrainte UNIQUE enlevée sur les réponses

--- db/migrations/0011_create_answers_table.sql
+++ db/migrations/0011_create_answers_table.sql
@@ -3,7 +3,7 @@ CREATE TABLE answers (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     question_id INTEGER NOT NULL,
     state INTEGER NOT NULL DEFAULT 0 CHECK (state IN (0, 1)),
-    content TEXT NOT NULL UNIQUE CHECK (length(trim(content)) > 0),
+    content TEXT NOT NULL CHECK (length(trim(content)) > 0),
     user_id INTEGER NOT NULL,
     FOREIGN KEY (question_id) REFERENCES questions(id) ON DELETE CASCADE,
     FOREIGN KEY (user_id) REFERENCES users(id)
